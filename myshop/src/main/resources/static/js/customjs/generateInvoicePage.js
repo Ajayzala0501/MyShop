a7a41793c4ef2ccf294db1f8a8b8d5ac271843af
AJAY ZALA <ajjuzala.007@gmail.com>
2022-12-06 23:56:42 +0530

    Create Invoice 1.8

--- myshop/src/main/resources/static/js/customjs/generateInvoicePage.js
+++ myshop/src/main/resources/static/js/customjs/generateInvoicePage.js
@@ -1,1 +1,11 @@
-console.log(localStorage.getItem("InvoiceDetails"));
+var data = JSON.parse(localStorage.getItem("InvoiceDetails"));
+$("#invoiceID").html(data[0]["invoiceID"]);
+$("#customerName").html(data[0]["customerName"]);
+$("#customerAddress").html(data[0]["customerAddress"]);
+$("#customerMobileNumber").html(data[0]["customerMobileNumber"]);
+$("#invoiceDate").html(data[0]["invoiceDate"]);
+$("#invoiceSubTotal").html(data[0]["invoiceSubTotal"]);
+$("#invoiceTax").html(data[0]["invoiceTax"]);
+$("#invoiceTotal").html(data[0]["invoiceTotal"]);
+
+console.log(data);
